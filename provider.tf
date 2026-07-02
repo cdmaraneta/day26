389ebefd4ab342c7aa6d14079c9e6bc847fc1563
Nero <65642721+cdmaraneta@users.noreply.github.com>
2026-07-03 03:16:12 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,6 +13,10 @@ terraform {
       source = "hashicorp/azuread"
       version = "~> 3.0.2"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0" # Use the latest version
+    }
   }
 
   required_version = ">= 1.9.0"
